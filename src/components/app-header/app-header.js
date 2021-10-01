7060d3d0fe1efc483387d64b1bab2e5efb58fd61
Anton1791 <anton.syryza@gmail.com>
2021-10-01 14:11:04 +0300

    update name

--- src/components/app-header/app-header.js
+++ src/components/app-header/app-header.js
@@ -4,7 +4,7 @@ import './app-header.css';
 const AppHeader = ({allPost, liked}) => {
     return (
         <div className="app-header d-flex">
-            <h1>Anton Syrytsia</h1>
+            <h1>User name!</h1>
             <h2>{allPost} записей, из них понравилось {liked}</h2>
         </div>
     )
